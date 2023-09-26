8f32f37329bfe1a661035c2ca9f461c4da339c3a
Mamduh Halawa <mamduhkod@gmail.com>
2023-09-26 22:39:13 +0200

    first

---
+++ read_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('/Users/mamduhhalawa/Desktop/World_data/world-data-2023.csv')
+df.info()
+df.describe()
+df.columns
+
+# Which factors are most associatd with high CO-2 emissions?
+df.sort_values(by='Co2-Emissions')
+
+
+
+
+
